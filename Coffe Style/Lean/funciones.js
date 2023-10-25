b46867dfe8cdcbddd8cc501f0634a097cd8e1ae7
lenase0077 <leandroserrano777@gmail.com>
2023-10-25 15:57:56 -0300

    Pull de cambios imagenes
    Cambios en el navbar, funciones js y logos en carpeta

--- Coffe Style/Lean/funciones.js
+++ Coffe Style/Lean/funciones.js
@@ -1,58 +1,56 @@
-let intervalId; // Variable para almacenar el ID del temporizador
-
-if (document.querySelector("#container-slider")) {
-  startAutoSlide(); // Iniciar el desplazamiento automático al cargar la página
+// Verifica la existencia del contenedor del slider
+if (document.querySelector('#container-slider')) {
+  // Establece un intervalo para avanzar automáticamente el slider
+  setInterval(function () {
+    funcionEjecutar('siguiente');
+  }, 15000);
 }
 
-//------------------------------ LIST SLIDER -------------------------
-const listSlider = document.querySelector(".listslider");
-if (listSlider) {
-  const links = listSlider.querySelectorAll("li a");
-  links.forEach((link) => {
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
-      const item = link.getAttribute("itlist");
-      const [, side] = item.split("_");
-      funcionEjecutar(side);
-      clearInterval(intervalId); // Detener el temporizador al interactuar manualmente
-      startAutoSlide(); // Reiniciar el desplazamiento automático
+// Verifica la existencia de elementos con la clase 'listslider'
+if (document.querySelector('.listslider')) {
+  // Obtiene todos los enlaces dentro de elementos de lista
+  const links = document.querySelectorAll('.listslider li a');
+  
+  // Adjunta manejadores de eventos 'click' a los enlaces
+  links.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault(); // Corrige el error tipográfico "anteriorentDefault"
+      const item = this.getAttribute('itlist');
+      const arrItem = item.split('_');
+      funcionEjecutar(arrItem[1]);
       return false;
     });
   });
 }
 
-function startAutoSlide() {
-  intervalId = setInterval(() => {
-    funcionEjecutar("siguiente");
-  }, 20000);
-}
-
+// Función para controlar el slider
 function funcionEjecutar(side) {
-  const parentTarget = document.getElementById("slider");
-  const elements = parentTarget.getElementsByTagName("li");
+  const parentTarget = document.getElementById('slider');
+  const elements = parentTarget.getElementsByTagName('li');
   let curElement, siguienteElement;
 
+  // Encuentra el elemento actual
   for (let i = 0; i < elements.length; i++) {
-    if (elements[i].style.opacity == 1) {
+    if (elements[i].style.opacity === '1') {
       curElement = i;
       break;
     }
   }
-  if (side == "anterior" || side == "siguiente") {
-    if (side == "anterior") {
-      siguienteElement = curElement === 0 ? elements.length - 1 : curElement - 1;
-    } else {
-      siguienteElement = curElement === elements.length - 1 ? 0 : curElement + 1;
-    }
+
+  // Determina el siguiente elemento según la dirección ('anterior', 'siguiente' o un número)
+  if (side === 'anterior' || side === 'siguiente') {
+    siguienteElement = (side === 'anterior') ? (curElement === 0 ? elements.length - 1 : curElement - 1) : (curElement === elements.length - 1 ? 0 : curElement + 1);
   } else {
     siguienteElement = side;
-    side = curElement > siguienteElement ? "anterior" : "siguiente";
+    side = (curElement > siguienteElement) ? 'anterior' : 'siguiente';
   }
 
-  //PUNTOS INFERIORES
-  const elementSel = document.querySelector(".listslider").querySelectorAll("a");
-  elementSel[curElement].classList.remove("item-select-slid");
-  elementSel[siguienteElement].classList.add("item-select-slid");
+  // Actualiza la lista de puntos inferiores
+  const elementSel = document.querySelector('.listslider').getElementsByTagName('a');
+  elementSel[curElement].classList.remove('item-select-slid');
+  elementSel[siguienteElement].classList.add('item-select-slid');
+
+  // Actualiza la visibilidad y el orden Z de los elementos del slider
   elements[curElement].style.opacity = 0;
   elements[curElement].style.zIndex = 0;
   elements[siguienteElement].style.opacity = 1;
