96fbba3bbc84a199586ccf5301703468c8fa7b59
Soledad Mello <mello.soledad@gmail.com>
2023-12-12 16:59:13 -0300

    update

--- Coffe Style/BackEnd/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('comentaBtn').addEventListener('click', openPopup);
-
-function openPopup() {
-    document.getElementById('comentaPopup').style.display = 'block';
-}
-
-function closePopup() {
-    document.getElementById('comentaPopup').style.display = 'none';
-}
