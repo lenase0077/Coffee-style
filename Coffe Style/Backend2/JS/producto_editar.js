7d404618879aa8ca128fe90bb7e6b6155b2de0bf
lenase0077 <leandroserrano777@gmail.com>
2023-12-15 19:34:02 -0300

    AAAAAAA
    a

--- Coffe Style/Backend2/JS/producto_editar.js
+++ Coffe Style/Backend2/JS/producto_editar.js
@@ -1,61 +1,59 @@
-console.log(location.search) // lee los argumentos pasados a este formulario
-var id=location.search.substr(4)
-console.log(id)
+const params = new URLSearchParams(window.location.search);
+const id = params.get('id');
+
 const { createApp } = Vue
 createApp({
-data() {
-return {
-id:0,
-nombre:"",
-imagen:"",
-stock:0,
-precio:0,
-url:'https://zole.pythonanywhere.com/productos'+id,
-}
-},
-methods: {
-fetchData(url) {
-fetch(url)
-.then(response => response.json())
-.then(data => {
-
-console.log(data)
-this.id=data.id
-this.nombre = data.nombre;
-this.imagen=data.imagen
-this.stock=data.stock
-this.precio=data.precio
-})
-.catch(err => {
-console.error(err);
-this.error=true
-})
-},
-modificar() {
-let producto = {
-nombre:this.nombre,
-precio: this.precio,
-stock: this.stock,
-imagen:this.imagen
-}
-var options = {
-body: JSON.stringify(producto),
-method: 'PUT',
-headers: { 'Content-Type': 'application/json' },
-redirect: 'follow'
-}
-fetch(this.url, options)
-.then(function () {
-alert("Registro modificado")
-window.location.href = "./productos.html";
-})
-.catch(err => {
-console.error(err);
-alert("Error al Modificar")
-})
-}
-},
-created() {
-this.fetchData(this.url)
-},
-}).mount('#app')
+  data() {
+    return {
+      id: 0,
+      nombre: "",
+      imagen: "",
+      stock: 0,
+      precio: 0,
+      url: `https://zole.pythonanywhere.com/productos/${id}`,
+    }
+  },
+  methods: {
+    fetchData(url) {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          this.id = data.id;
+          this.nombre = data.nombre;
+          this.imagen = data.imagen;
+          this.stock = data.stock;
+          this.precio = data.precio;
+        })
+        .catch(err => {
+          console.error(err);
+          this.error = true;
+        })
+    },
+    modificar() {
+      let producto = {
+        nombre: this.nombre,
+        precio: this.precio,
+        stock: this.stock,
+        imagen: this.imagen
+      }
+      var options = {
+        body: JSON.stringify(producto),
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        redirect: 'follow'
+      }
+      fetch(this.url, options)
+        .then(() => {
+          alert("Registro modificado");
+          window.location.href = "./productos.html";
+        })
+        .catch(err => {
+          console.error(err);
+          alert("Error al Modificar");
+        })
+    }
+  },
+  created() {
+    this.fetchData(this.url);
+  }
+}).mount('#app');
